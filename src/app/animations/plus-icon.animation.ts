2c9972016a1c74a429411d994946e8b0380aae60
pradeep <pradeep12191@gmail.com>
2019-08-03 10:57:11 -0700

    mobile table

--- src/app/animations/plus-icon.animation.ts
+++ src/app/animations/plus-icon.animation.ts
@@ -1,7 +1,14 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, animation } from '@angular/animations';
+
 
 export const ROTATE_PLUS_ANIMATION = trigger('rotatePlus', [
     state('collapsed', style({ transform: 'rotate(0deg)' })),
     state('expanded', style({ transform: 'rotate(90deg)' })),
+    transition('collapsed <=> expanded', animate('0.5s ease-in-out')),
+])
+
+export const ROTATE_ARROW_ANIMATION = trigger('rotateArrow', [
+    state('collapsed', style({ transform: 'rotate(0deg)' })),
+    state('expanded', style({ transform: 'rotate(180deg)' })),
     transition('collapsed <=> expanded', animate('0.5s ease-in-out')),
 ])
--- src/app/post-login/master/bit/view-bit/view-bit.component.ts
+++ src/app/post-login/master/bit/view-bit/view-bit.component.ts
@@ -2,23 +2,43 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Bit } from '../Bit';
 import { MatTableDataSource } from '@angular/material';
+import { MediaObserver } from '@angular/flex-layout';
+import { EXPAND, ROTATE_ARROW_ANIMATION } from '../../../../animations';
 
 @Component({
     templateUrl: './view-bit.component.html',
-    styleUrls: ['./view-bit.component.scss']
+    styleUrls: ['./view-bit.component.scss'],
+    animations: [EXPAND, ROTATE_ARROW_ANIMATION]
 })
 export class ViewBitComponent {
     bits: Bit[];
+    expandedBit;
     bitDataSource: MatTableDataSource<Bit>;
-    displayedColumns: string[] = ['serialNo', 'name', 'size', 'number', 'purchaseDate', 'type', 'drilling', 'distributorDetails', 'intialScale', 'edit', 'delete', ];
+    displayedColumns: string[] = ['serialNo', 'name', 'size', 'number', 'purchaseDate', 'type', 'drilling', 'distributorDetails', 'intialScale', 'edit', 'delete',];
+    sticky = true;
+    isMobile = false;
     constructor(
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private mediaObs: MediaObserver
     ) {
         this.route.data.subscribe((data: { bits: Bit[] }) => {
             if (data && data.bits) {
                 this.bits = data.bits;
                 this.bitDataSource = new MatTableDataSource<Bit>(this.bits);
             }
+        });
+        
+        if (this.mediaObs.isActive('lt-md')) {
+            this.displayedColumns = ['serialNo', 'name', 'size', 'edit', 'delete', 'more_details'];
+            this.sticky = false;
+            this.isMobile = true;
+        }
+
+    }
+
+    private addMoreDetailsColumn() {
+        this.bits.forEach(() => {
+
         })
     }
 

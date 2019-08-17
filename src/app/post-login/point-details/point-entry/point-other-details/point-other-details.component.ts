63ff95d5976c64fe3474ef0aa43873bd102d5793
pradeep <pradeep12191@gmail.com>
2019-08-16 22:50:58 -0700

    total rpm display at other ddetails

--- src/app/post-login/point-details/point-entry/point-other-details/point-other-details.component.ts
+++ src/app/post-login/point-details/point-entry/point-other-details/point-other-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { FormGroup } from '@angular/forms';
 import { ConfigService } from '../../../../services/config.service';
 
@@ -7,12 +7,18 @@ import { ConfigService } from '../../../../services/config.service';
     templateUrl: './point-other-details.component.html',
     styleUrls: ['./point-other-details.component.scss']
 })
-export class PointOtherDetailsComponent {
+export class PointOtherDetailsComponent implements OnInit {
     appearance;
     @Input() form: FormGroup;
+    totalRpm;
     constructor(
         private config: ConfigService
-    ){
+    ) {
         this.appearance = this.config.getConfig('formAppearance');
+    }
+
+    ngOnInit() {
+        console.log(this.form.parent.get('point.bitDetails.totalRpm'));
+        this.totalRpm = this.form.parent.get('point.bitDetails.totalRpm').value;
     }
 }
